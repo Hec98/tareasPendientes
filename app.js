90d3f99c915182e79a7ed089d8c19ad01118c590
Héctor De La Torre Jiménez <47239028+Hec98@users.noreply.github.com>
2021-07-16 17:13:31 -0500

    Repeat basic menu infinitely

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const { mostrarMenu, pausa } = require('./helpers/mensajes')
 console.clear()
 
 const main = async() => {
-  console.log('Hola Mundo');
-  mostrarMenu();
-  // pausa();
+  let opt = '';
+
+  do {
+    opt = await mostrarMenu();
+    console.log({opt})
+    if(opt !== '0') await pausa();
+  } while(opt !== '0');
 }
 
 main();
