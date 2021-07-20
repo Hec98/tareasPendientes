915b5deb331f371f1edea1bdd24dbc1d046e8d8a
Héctor De La Torre Jiménez <47239028+Hec98@users.noreply.github.com>
2021-07-20 17:41:05 -0500

    Task implementation example

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 require('colors');
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
-
+const Tarea = require('./models/tarea')
+const Tareas = require('./models/tareas')
 console.clear()
 
 const main = async() => {
   let opt = '';
 
   do {
-    opt = await inquirerMenu();
-    console.log({opt})
+    // opt = await inquirerMenu();
+    // console.log({opt})
+    
+    const tarea = new Tarea('Comprar comida');
+    // console.log(tarea)
+    const tareas = new Tareas();
+    tareas._listado[tarea.id] = tarea;
+    console.log(tareas)
+
     await pausa();
   } while(opt !== '0');
 }
