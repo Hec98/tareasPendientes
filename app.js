1b0df8c29127923fd93b2ef23d8e80f54cb8850e
Héctor De La Torre Jiménez <47239028+Hec98@users.noreply.github.com>
2021-07-20 21:33:49 -0500

    Call leerInput Add option switch Create and list tasks

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 require('colors');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const { 
+  inquirerMenu, 
+  pausa,
+  leerInput
+} = require('./helpers/inquirer');
 const Tareas = require('./models/tareas')
 
 const main = async() => {
   let opt = '';
+  const tareas = new Tareas();
 
   do {
     opt = await inquirerMenu();
-    console.log({opt})
+    switch(opt) {
+      case '1':
+        // Crear opción
+        const desc = await leerInput('Descripción: ');
+        tareas.crearTarea(desc);
+      break;
+      case '2':
+        console.log(tareas._listado);
+      break;
+
+    }
 
     await pausa();
   } while(opt !== '0');
