2fb3ea2e28f712a89eed8932fc652547c9309317
Héctor De La Torre Jiménez <47239028+Hec98@users.noreply.github.com>
2021-07-20 22:07:52 -0500

    Add get listadoArr()

--- models/tareas.js
+++ models/tareas.js
@@ -3,6 +3,15 @@ const Tarea = require('./tarea');
 class Tareas {
   _listado = {};
   
+  get listadoArr() {
+    const listado = [];
+    Object.keys(this._listado).forEach(key => {
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+    return listado;
+  }
+
   constructor() {
     this._listado = {};
   }
@@ -11,7 +20,6 @@ class Tareas {
     const tarea = new Tarea(desc);
     this._listado[tarea.id] = tarea;
   }
-
 }
 
 module.exports = Tareas;
