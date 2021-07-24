c0d94a92c307c44b43cd83cc9a2c0e8d273eb2a5
Hec98 <hector982015@gmail.com>
2021-07-23 23:56:16 -0500

    Task load

--- app.js
+++ app.js
@@ -14,11 +14,9 @@ const main = async() => {
 
   const tareasDB = leerDB();
 
-  if(tareasDB) {
-    // Establecer tareas
+  if(tareasDB) { // Cargar tareas
+    tareas.cargarTareasFromArray(tareasDB);
   }
-
-  await pausa();
 
   do {
     opt = await inquirerMenu();
@@ -33,7 +31,7 @@ const main = async() => {
       break;
     }
     
-    // guardarDB(tareas.listadoArr);
+    guardarDB(tareas.listadoArr);
     
     await pausa();
   } while(opt !== '0');
